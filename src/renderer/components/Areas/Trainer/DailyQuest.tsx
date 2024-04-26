19a191a8c2a89c34171920f707cc50d1a32b58cc
Edo Mathias <edo.mathias@gmail.com>
2024-04-26 13:41:26 +0300

    Added bare-bones quest component that gets your current exercises

--- src/renderer/components/Areas/Trainer/DailyQuest.tsx
+++ src/renderer/components/Areas/Trainer/DailyQuest.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ExerciseModel from '../../../../models/exercise-model';
 
 function DailyQuest() {
-  return <div>DailyQuest</div>;
+  const [dailyQuest, setDailyQuest] = useState<ExerciseModel[]>([]);
+
+  useEffect(() => {
+    const getExercises = async () => {
+      try {
+        const exercises = await window.trainerApi.getExercises();
+        console.log('Exercises:', exercises);
+        setDailyQuest(exercises);
+      } catch (error) {
+        console.error('Error fetching exercises:', error);
+      }
+    };
+
+    getExercises();
+  }, []);
+
+  return (
+    <div>
+      <h1>DAILY QUEST: Strength Training</h1>
+      <hr />
+      <ol>
+        {dailyQuest?.map((exercise, index) => (
+          <li key={index}>
+            {exercise.name} - {exercise.repetitions} reps
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 export default DailyQuest;
