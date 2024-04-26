fb245d0aa7b6192523168f541cc7a669f210494f
Edo Mathias <edo.mathias@gmail.com>
2024-04-26 13:40:22 +0300

    Settings page will now use the timer and exercise form components

--- src/renderer/components/Areas/Settings/Settings.tsx
+++ src/renderer/components/Areas/Settings/Settings.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import ExerciseForm from './ExerciseForm';
+import Timer from './Timer';
 
 function Settings() {
-  return <div>Settings</div>;
+  return (
+    <div>
+      <h1>Settings</h1>
+      <hr />
+      <Timer />
+      <hr />
+      <label>Daily quests:</label>
+      <ExerciseForm />
+    </div>
+  );
 }
 
 export default Settings;
