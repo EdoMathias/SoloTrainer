fb0abb5c714f7901574f518a483ead597cac21b5
Edo Mathias <edo.mathias@gmail.com>
2024-04-26 13:37:51 +0300

    Added global declarations so the renderer process can use the trainerApis

---
+++ src/declarations.d.ts
@@ -0,0 +1,21 @@
+import { ExerciseModel } from '.';
+
+export interface ITrainerApi {
+  setExercises: (exercises: ExerciseModel[]) => void;
+  getExercises: () => Promise<ExerciseModel[]>;
+}
+
+export interface IIpcRenderer {
+  on: (
+    channel: string,
+    listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void
+  ) => void;
+  removeAllListeners: (channel: string) => void;
+}
+
+declare global {
+  interface Window {
+    trainerApi: ITrainerApi;
+    ipcRenderer: IIpcRenderer;
+  }
+}
