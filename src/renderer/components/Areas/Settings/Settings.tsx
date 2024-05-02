914cac8b50ffe593e943573b78d31dfb99cc5e5e
Edo Mathias <edo.mathias@gmail.com>
2024-05-02 21:04:33 +0300

    Added a button to go back to the main menu from the settings page

--- src/renderer/components/Areas/Settings/Settings.tsx
+++ src/renderer/components/Areas/Settings/Settings.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ExerciseForm from './ExerciseForm';
-import Timer from './Timer';
+import React from "react";
+import ExerciseForm from "./ExerciseForm";
+import Timer from "./Timer";
+import MenuButton from "../../MenuButton/MenuButton";
 
 function Settings() {
   return (
     <div>
       <h1>Settings</h1>
+      <MenuButton navTo="mainMenu" name="Main Menu" />
       <hr />
       <Timer />
       <hr />
