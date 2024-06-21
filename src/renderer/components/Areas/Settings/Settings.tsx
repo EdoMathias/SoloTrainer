63217215bb560c3702b381bf2c3da9ab90403d91
Edo Mathias <edo.mathias@gmail.com>
2024-06-21 18:15:43 +0300

    Settings page redesign

--- src/renderer/components/Areas/Settings/Settings.tsx
+++ src/renderer/components/Areas/Settings/Settings.tsx
@@ -1,16 +1,16 @@
 import ExerciseForm from "./ExerciseForm";
 import MenuButton from "../../MenuButton/MenuButton";
 import TimerForm from "../../Timer/TimerForm";
+import "./Settings.css";
 
 function Settings() {
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className="settings-container">
+      <h1 className="settings-title">Settings</h1>
       <MenuButton navTo="mainMenu" name="Main Menu" />
       <hr />
       <TimerForm />
       <hr />
-      <label>Daily quests:</label>
       <ExerciseForm />
     </div>
   );
