d338bd76894836444b2215fcd0ae1fd733caefaa
Edo Mathias <edo.mathias@gmail.com>
2024-06-22 19:34:36 +0300

    Added tray icon, removed auto dev tools open

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,20 +1,31 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import path from "path";
 
 export const mainConfig: Configuration = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/main/main.ts',
+  entry: "./src/main/main.ts",
   // Put your normal webpack config below here
   module: {
     rules,
   },
-  plugins,
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, ".webpack/assets"),
+        },
+      ],
+    }),
+  ],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
 };
